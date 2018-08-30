1b70d4c67e5ef50e3facb0a2c19c9f3a63d6babb
Ilia Choly <icholy@accipiterradar.com>
2018-08-30 12:48:30 -0400

    start working on the map interface

--- slippy.go
+++ slippy.go
@@ -17,6 +17,48 @@ var (
 	)
 )
 
+type Options struct {
+	Lat, Lon float64
+	Zoom     int
+	Bounds   pixel.Rect
+}
+
+type Map struct {
+	opts   Options
+	origin pixel.Vec
+	area   pixel.Rect
+	tiles  []ImageTile
+}
+
+func New(opts Options) *Map {
+	var (
+		origin = tiles.Vec(opts.Lat, opts.Lon, opts.Zoom)
+		area   = opts.Bounds.Moved(origin)
+	)
+	return &Map{
+		opts:   opts,
+		origin: origin,
+		area:   area,
+		tiles:  RectTiles(area, opts.Zoom),
+	}
+}
+
+func (m *Map) Fetch() error {
+	for i := range m.tiles {
+		if err := m.tiles[i].Fetch(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (m *Map) Draw(tg pixel.Target) {
+	reset := pixel.IM.Moved(pixel.ZV.Sub(m.origin))
+	for _, t := range m.tiles {
+		t.Draw(tg, reset)
+	}
+}
+
 func TilePictureData(t tiles.Tile) (*pixel.PictureData, error) {
 	url := URL(t)
 	img, err := util.FetchImage(url)
