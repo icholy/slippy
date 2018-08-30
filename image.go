96de2b2fe7fc163966065799e512e12cdf93a4c2
Ilia Choly <icholy@accipiterradar.com>
2018-08-30 11:22:55 -0400

    update image

--- image.go
+++ image.go
@@ -16,15 +16,41 @@ var (
 
 type ImageTile struct {
 	tiles.Tile
-	Pic    *pixel.PictureData
+	Sprite *pixel.Sprite
 	Offset pixel.Vec
+	Frame  pixel.Rect
+}
+
+func NewImageTile(t tiles.Tile, bounds pixel.Rect) ImageTile {
+	frame := t.Rect().Intersect(bounds)
+	return ImageTile{
+		Tile:   t,
+		Sprite: pixel.NewSprite(Placeholder, frame),
+		Offset: bounds.Min.To(frame.Min),
+		Frame:  frame,
+	}
+}
+
+func (t ImageTile) Fetch() error {
+	pic, err := TilePictureData(t.Tile)
+	if err != nil {
+		return err
+	}
+	t.Sprite = pixel.NewSprite(pic, pic.Bounds())
+	return nil
+}
+
+func (t ImageTile) Draw(tg pixel.Target, m pixel.Matrix) {
+	half := float64(tiles.TileSize) / 2
+	v := t.Vec().Add(pixel.V(half, half))
+	t.Sprite.Draw(tg, pixel.IM.Moved(v).Chained(m))
 }
 
 // RectTiles returns a slice of tiles requires to fully cover the rect
-func RectTiles(r pixel.Rect, zoom int) []ImageTile {
+func RectTiles(bounds pixel.Rect, zoom int) []ImageTile {
 	var (
-		min = tiles.FromVec(r.Min, zoom)
-		max = tiles.FromVec(r.Max, zoom)
+		min = tiles.FromVec(bounds.Min, zoom)
+		max = tiles.FromVec(bounds.Max, zoom)
 		tt  []ImageTile
 	)
 	for x := min.X; x <= max.X; x++ {
@@ -34,10 +60,7 @@ func RectTiles(r pixel.Rect, zoom int) []ImageTile {
 				Y: y,
 				Z: zoom,
 			}
-			tt = append(tt, ImageTile{
-				Tile: t,
-				Pic:  Placeholder,
-			})
+			tt = append(tt, NewImageTile(t, bounds))
 		}
 	}
 	return tt
